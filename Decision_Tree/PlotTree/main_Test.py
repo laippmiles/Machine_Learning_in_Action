da586100a90c4ce629e156273d9c8c9b03c1f1d9
laipp123 <laippmiles@gmail.com>
2018-02-28 15:11:20 +0800

    Decision_Tree

--- Decision_Tree/PlotTree/main_Test.py
+++ Decision_Tree/PlotTree/main_Test.py
@@ -1,4 +1,9 @@
 from retrieveTree import *
 from getNumLeafs import *
+from getTreeDepth import *
+#from treePlotter import *
+from treePlot4In1 import *
 tree = retrueveTree(0)
 print(getNumLeafs(tree))
+print(getTreeDepth(tree))
+createPlot(tree)
