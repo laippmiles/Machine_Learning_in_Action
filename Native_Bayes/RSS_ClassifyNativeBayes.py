18ff256836a89dab644eac76c219f79e47a03b4b
laipp123 <laippmiles@gmail.com>
2018-03-02 18:42:43 +0800

    Native_Bayes

--- Native_Bayes/RSS_ClassifyNativeBayes.py
+++ Native_Bayes/RSS_ClassifyNativeBayes.py
@@ -1,4 +1,7 @@
+#现实用RSS爬虫并做朴素贝叶斯分析
+from feedparser import parse
 from textParse import *
+#textParse(string)
 from loadDataSet import *
 #loadDataSet()
 from createVocabList import *
@@ -8,37 +11,40 @@ from words2Vector import *
 from nativeBayes import *
 #trainNativeBayes(trainData,trainLabel)  ;return p1Vector , p0Vector, pClass1
 #classifyNativeBayes(inputData,p1Vector,p0Vector,pClass1)  ,
-#print(textParse(r"Hello world , I'm a corgi."))
-#测试分词功能
-from os import listdir
 from random import uniform
-listOfHamEmail = listdir('email/ham')
-numOfHamEmail = len(listOfHamEmail)
 
-listOfSpamEmail = listdir('email/spam')
-numOfSpamEmail = len(listOfSpamEmail)
+print('Loading RSS data')
+fromSfBay = parse('https://sfbay.craigslist.org/stp/index.rss')
+fromNewYork = parse('https://newyork.craigslist.org/stp/index.rss')
+print('Complate')
+#就一行就能把数据读回来，简直厉害
 
-emailList = []
+postList = []
 classList = []
+#1:from SfBay
+#0:from NewYork
+fullText = []
 
-for i in range(1,numOfSpamEmail+1):
-    #这里记得+1
-    email = open('email/spam/%d.txt' %i, "rb").read().decode('GBK', 'ignore')
-    #这个对比书上的代码要麻烦一点，属于python2和python3之间编码差异问题
-    # wordList=textParse(open('email/spam/%d.txt' %i).read()) 照书写会 unicode error
-    wordList = textParse(email)
-    emailList.append(wordList)
+numOfSfBay = len(fromSfBay['entries'])
+numOfNewYork = len(fromNewYork['entries'])
+
+for dataSfBay in range(numOfSfBay):
+    post = fromSfBay['entries'][dataSfBay]['summary']
+    #parse读出来是一个嵌套字典的形式，用这种办法提取字符串
+    post = textParse(post)
+    postList.append(post)
     classList.append(1)
 
-for i in range(1,numOfHamEmail+1):
-    email = open('email/ham/%d.txt' %i, "rb").read().decode('GBK', 'ignore')
-    wordList = textParse(email)
-    emailList.append(wordList)
+for dataNewYork in range(numOfNewYork):
+    post = fromNewYork['entries'][dataNewYork]['summary']
+    post = textParse(post)
+    postList.append(post)
     classList.append(0)
 
-vocabList = createVocabList(emailList)
+#----------------------------------从这开始除了数据集变量不同，和mainTestEmail相应部分是一样的------------------------------
+vocabList = createVocabList(postList)
 
-trainSetIndex = list(range(len(emailList)))
+trainSetIndex = list(range(len(postList)))
 #注意python2与python3 range的差别
 #python3的range出来不是list，而是一个迭代变量
 testSetIndex = []
@@ -57,7 +63,7 @@ testData = []
 testLabel = []
 
 for indexTrain in trainSetIndex:
-    trainData.append(bagOfWords2Vector(vocabList,emailList[indexTrain]))
+    trainData.append(bagOfWords2Vector(vocabList,postList[indexTrain]))
     #记得不要直接导入文本，要调用Words2Vector转换成数字矩阵形式
     trainLabel.append(classList[indexTrain])
 
@@ -67,10 +73,10 @@ error = 0.0
 
 #指标用的准确率
 for indexTest in testSetIndex:
-    testVector = bagOfWords2Vector(vocabList,emailList[indexTest])
+    testVector = bagOfWords2Vector(vocabList,postList[indexTest])
     answer = classifyNativeBayes(testVector,p1Vector , p0Vector, pClass1)
     if answer != classList[indexTest]:
         error += 1
 
-accuracy = 1 - float(error / len(emailList))
+accuracy = 1 - float(error / len(postList))
 print('The accuracy is : %f' % accuracy)
--- Native_Bayes/textParse.py
+++ Native_Bayes/textParse.py
@@ -7,6 +7,7 @@ def textParse(string):
     #多个分隔符，复杂的分隔情况，使用re.split
     #\W*,指配对一连串字母数字知道出现标点符号为止
     return [tokens.lower() for tokens in listOfTokens if len(tokens) > 2 ]
+    #只留下长度大于2的词汇
     #l上面使用了推导式
     #详见https://www.cnblogs.com/tkqasn/p/5977653.html
     #简单地说就是先for in 再在后面追加if
