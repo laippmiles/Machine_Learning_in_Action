9364ecfd5ba7e400f61061d15f61abd8a607078b
laipp123 <laippmiles@gmail.com>
2018-03-03 22:15:10 +0800

    Decision_Tree

--- Decision_Tree_ID3/main_Test_Decision_Tree.py
+++ Decision_Tree_ID3/main_Test_Decision_Tree.py
@@ -1,10 +1,9 @@
-from csv2ListOrMatrix import *
-from shannonEnt import *
-from splitDataSet import *
 from chooseBestFeatureToSplit import *
 from createTree import *
-from decisionTreeClassify import *
+from csv2ListOrMatrix import *
+from shannonEnt import *
 from treePlot4In1 import *
+
 datasetName = 'fish'
 data = csv2ListOrMatrix('Test_'+ datasetName +'.csv','list')
 labels = csv2ListOrMatrix('Test_labels_' + datasetName + '.csv','list')[0]
