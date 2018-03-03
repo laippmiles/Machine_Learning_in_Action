af4a9aab6db65853c68d31d392c23f1e1578a55f
laipp123 <laippmiles@gmail.com>
2018-03-03 22:16:31 +0800

    KNN

--- KNN/Main_DatingClassify.py
+++ KNN/Main_DatingClassify.py
@@ -1,7 +1,9 @@
-from  File2Matrix import *
-from  AutoNorm import *
-from  K_NN import *
 from numpy import array
+
+from  AutoNorm import *
+from  File2Matrix import *
+from  K_NN import *
+
 Mat,Lables = File2Matrix('datingTestSet.txt',3)
 NormDataSet,MaxMinusMin,MinVals = AutoNorm(Mat)
 #print(NormDataSet)
