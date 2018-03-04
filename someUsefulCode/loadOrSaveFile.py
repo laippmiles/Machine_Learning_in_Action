97a0f2de1dccb7f8b9c3d856ef378d75f944f71a
laipp123 <laippmiles@gmail.com>
2018-03-04 21:26:56 +0800

    someUsefulCode

--- someUsefulCode/loadOrSaveFile.py
+++ someUsefulCode/loadOrSaveFile.py
@@ -1,12 +1,12 @@
 from pickle import dump,load
 from shutil import move
-def saveFile(data,fileName):
+def saveFile(data,newpath,fileName):
     fw = open(fileName,'wb')
     #write() argument must be str, not bytes出现这个的时候，将‘w’改为‘wb’即可
     dump(data,fw)
     fw.close()
-    newpath = 'dataOfRSS\\' + fileName
-    move(fileName,newpath)
+    move(fileName,newpath + '\\' + fileName)
+    #这里字符串前加不加r好像都可以达成目的，不懂为毛
 
 def loadFile(fileName):
     fw = open(fileName,'rb')
